7df7c261dcea698ad9d9fa59e17170900ed0e236
Thomas Goyne <tgoyne@gmail.com>
2012-06-28 21:11:52 -0700

    Fetch the bootstrap data and start polling the stream

--- ExampleClient/ExampleClient/LFCMainViewController.m
+++ ExampleClient/ExampleClient/LFCMainViewController.m
@@ -9,34 +9,48 @@
 #import "LFCMainViewController.h"
 
 @interface LFCMainViewController ()
-
+@property (strong, nonatomic) LivefyreClient *client;
+@property (strong, nonatomic) Collection *collection;
+@property (strong, nonatomic) RequestComplete gotData;
 @end
 
 @implementation LFCMainViewController
+@synthesize client = _client;
+@synthesize collection = _collection;
+@synthesize gotData = _gotData;
 
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+- (id)init {
+    self = [super init];
+    if (self) {
+        self.gotData = ^(BOOL error, id resultOrError) {
+            if (error) {
+                [[[UIAlertView alloc] initWithTitle:@"Error"
+                                            message:resultOrError
+                                           delegate:nil
+                                  cancelButtonTitle:@"OK"
+                                  otherButtonTitles:nil,
+                  nil] show];
+            }
+        };
+    }
+    return self;
 }
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-}
-
-#pragma mark - Flipside View
 
 - (void)flipsideViewControllerDidFinish:(LFCFlipsideViewController *)controller
 {
     [self dismissModalViewControllerAnimated:YES];
+
+    if (self.client) {
+        [self.client stopPollingForUpdates:self.collection];
+    }
+
+    self.client = controller.client;
+    self.collection = controller.collection;
+    [self.collection fetchBootstrap:self.gotData];
+    [self.client startPollingForUpdates:self.collection
+                          pollFrequency:30
+                         requestTimeout:30
+                            gotNewPosts:self.gotData];
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
